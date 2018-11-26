39a8317040d2e845e48b3ad91fbd49f6313217e2
Paretkf <paretk7@gmail.com>
2018-11-26 19:39:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png"> <br>
+    <button @click="getUser()">get User from API</button> <br>
+    {{user}}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import {mapActions, mapState} from 'vuex'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    ...mapActions({
+      getUser: 'getUser'
+    })
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
